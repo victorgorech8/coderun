0a653c8763ec2d1c9205cf9af2f5c511f32f535a
Victor Gorunov <fullf305@gmail.com>
2023-12-13 23:45:33 +0700

    other variant

--- Downloads/coderun/317b.py
+++ Downloads/coderun/317b.py
@@ -1,8 +1,8 @@
 import sys
 import math
-def zet( i,r1,r2,r3):
-    z=i**3+    ((-2*(-r1+i)+(4*(-r1+i)**2-8*(r1**2-r2-2*r1*i+2*i**2))**(1/2))/4)**3+   (-(-2*(-r1+i)+(4*(-r1+i)**2-8*(r1**2-r2-2*r1*i+2*i**2))**(1/2))/4+r1-i)**3-r3
-    return z
+#def zet( i,r1,r2,r3):
+   # z=i**3+    ((-2*(-r1+i)+(4*(-r1+i)**2-8*(r1**2-r2-2*r1*i+2*i**2))**(1/2))/4)**3+   (-(-2*(-r1+i)+(4*(-r1+i)**2-8*(r1**2-r2-2*r1*i+2*i**2))**(1/2))/4+r1-i)**3-r3
+    #return z
 
 n=int(input())
 s1,s2,s3 = map(int,input().split())
@@ -13,48 +13,13 @@ r1=s11-s1
 r2=s21-s2
 r3=s31-s3
 h=math.ceil(r3**(1/3))+1
-i=math.floor(h/2)
-if (zet(n,r1,r2,r3)==0):
-    z=n
-    y=int((-2*(-r1+z)+(4*(-r1+z)**2-8*(r1**2-r2-2*r1*z+2*z**2))**(1/2))/4)
-    x=int(r1-z-y)
-    print(x,y,z)
-    sys.exit()
-
-if (zet(1,r1,r2,r3)==0):
-    z=1
-    y=int((-2*(-r1+z)+(4*(-r1+z)**2-8*(r1**2-r2-2*r1*z+2*z**2))**(1/2))/4)
-    x=int(r1-z-y)
-    print(x,y,z)  
-    sys.exit()
-a=1
-b=h   
-while(abs(a-b)>1):
-    if (zet(i,r1,r2,r3)==0):
+for i in  range(1,h+1):
+    if (4*(-r1+i)**2-8*(r1**2-r2-2*r1*i+2*i**2)>=0):
+        y=(-2*(-r1+i)+(4*(-r1+i)**2-8*(r1**2-r2-2*r1*i+2*i**2))**(1/2))/4
         z=i
-        y=int((-2*(-r1+z)+(4*(-r1+z)**2-8*(r1**2-r2-2*r1*z+2*z**2))**(1/2))/4)
-        x=int(r1-z-y)
-        print(x,y,z)
-        sys.exit()
-
-    if (zet(i,r1,r2,r3)<0):
-        a=i
-    if(zet(i,r1,r2,r3)>0):
-        b=i
-    i=a+math.floor(b-a)
-
-
-if (zet(a,r1,r2,r3)==0):
-    z=a
-    y=int((-2*(-r1+z)+(4*(-r1+z)**2-8*(r1**2-r2-2*r1*z+2*z**2))**(1/2))/4)
-    x=int(r1-z-y)
-    print(x,y,z)
-    sys.exit()
-
-if (zet(b,r1,r2,r3)==0):
-    z=b
-    y=int((-2*(-r1+z)+(4*(-r1+z)**2-8*(r1**2-r2-2*r1*z+2*z**2))**(1/2))/4)
-    x=int(r1-z-y)
-    print(x,y,z)  
-    sys.exit()    
+        x=r1-y-z
+        if (y%1==0 and x%1==0):
+            if (i**3+    ((-2*(-r1+i)+(4*(-r1+i)**2-8*(r1**2-r2-2*r1*i+2*i**2))**(1/2))/4)**3+   (-(-2*(-r1+i)+(4*(-r1+i)**2-8*(r1**2-r2-2*r1*i+2*i**2))**(1/2))/4+r1-i)**3==r3):
+                print(int(x),int(y),z)
+                sys.exit()
 
